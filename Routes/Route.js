278c9a69ce68f677c37288eebf869985ad791e1a
nomiekbaker <152894346+nomiekbaker@users.noreply.github.com>
2023-12-07 04:22:07 +0000

    switch to html and fixes

--- Routes/Route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const trackerRoutes = require('./tracker.js')
-
-router.use(trackerRoutes)
-
-module.exports = router
--- app.js
+++ app.js
@@ -1,63 +1,78 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-// const morgan = require('morgan')
 const mongoose = require('mongoose')
-const multer = require('multer')
-const upload = multer()
 
-mongoose.connect('mongodb+srv://nomiekb:Idunno2023@cluster0.ca9wu98.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://nomiekb:Idunno2023@cluster0.ca9wu98.mongodb.net/')
 
-// const db = mongoose.connection
+const db = mongoose.connection
+db.on('error', (err) => { console.log(err) })
+db.once('open', () => {
+  console.log('Connected to MongoDB')
+})
 
-const schemaData = mongoose.Schema({
+const schemaData = new mongoose.Schema({
   name: String,
   picture: String,
   isFriendly: Boolean,
   species: String
 })
-const pet = mongoose.model('pet', schemaData)
-module.exports = pet
+
+const Pet = mongoose.model('Pet', schemaData)
 
 const app = express()
 
-// comment this part out
-// app.use(morgan('dev')); app.use(bodyParser.urlencoded({ extended: true })); app.use(bodyParser.json())
-// const PORT = process.env.PORT || 3000
-// app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
+const PORT = 4000
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const routes = require('./Routes/Route')
-app.use('/', routes)
-
-// start server
-app.listen(3000, () => {
-  console.log('listeniing at port:3000')
-})
-
-app.set('view engine', 'pug')
-app.set('views', './views')
-
-app.get('/', function (req, res) {
-  res.render('view')
-})
-
-// for parsing multipart/form-data
-app.use(upload.array())
 app.use(express.static('public'))
 
-app.post('/', function (req, res) {
-  res.render('view')
+app.get('/pets', async (req, res) => {
+  try {
+    const pets = await Pet.find({})
+    res.json(pets)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 })
 
-// comment this part out
-// db.on('error', (err) => { console.log(err) })
-// db.once('open', () => { console.log('Database Connection Established') })
-// db.collection.find()
+app.post('/pets', async (req, res) => {
+  const { name, photo, species, friendly } = req.body
+  const newPet = new Pet({ name, photo, species, friendly })
 
-// const myForm = document.getElementById('myForm')
-// myForm.addEventListener('submit', (e) => {
-//   const name = document.getElementById('name')
+  try {
+    const savedPet = await newPet.save()
+    res.status(201).json(savedPet)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
 
-// })
+app.put('/pets/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, photo, species, friendly } = req.body
+
+  try {
+    const updatedPet = await Pet.findByIdAndUpdate(
+      id,
+      { name, photo, species, friendly },
+      { new: true }
+    )
+    res.json(updatedPet)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
+app.delete('/pets/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await Pet.findByIdAndDelete(id)
+    res.json({ message: 'Pet deleted successfully' })
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
